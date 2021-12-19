23cc71d3ed11ac6ed161ad4c39584fc7da2f4671
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-19 12:13:43 +0200

    login added

--- src/controllers/authController.go
+++ src/controllers/authController.go
@@ -15,7 +15,7 @@ func Register(c *fiber.Ctx) error {
 	var data map[string]string
 
 	if err := c.BodyParser(&data); err != nil {
-		return nil
+		return err
 	}
 
 	if data["password"] != data["password_confirm"] {
@@ -36,6 +36,34 @@ func Register(c *fiber.Ctx) error {
 	}
 
 	database.DB.Create(&user)
+
+	return c.JSON(user)
+}
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.Id == 0 {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Invalid Credentials",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Wrong password",
+		})
+	}
 
 	return c.JSON(user)
 }
