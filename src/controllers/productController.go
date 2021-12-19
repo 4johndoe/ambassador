d8cdc10650c868a937b73014b2098c96d9d56cfd
bogdan demchenko <bogdandemchenko@gmail.com>
2021-12-19 19:16:06 +0200

    added backend product search

--- src/controllers/productController.go
+++ src/controllers/productController.go
@@ -7,6 +7,7 @@ import (
 	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -83,12 +84,47 @@ func ProductsFrontend(c *fiber.Ctx) error {
 			panic(err)
 		}
 
-		if errKey := database.Cache.Set(ctx, "products_frontend", bytes, 30*time.Minute).Err(); err != nil {
-			panic(errKey)
-		}
+		database.Cache.Set(ctx, "products_frontend", bytes, 30*time.Minute)
 	} else {
 		json.Unmarshal([]byte(result), &products)
 	}
 
 	return c.JSON(products)
+}
+
+func ProductsBackend(c *fiber.Ctx) error {
+	var products []models.Product
+	var ctx = context.Background()
+
+	result, err := database.Cache.Get(ctx, "products_backend").Result()
+
+	if err != nil {
+		database.DB.Find(&products)
+
+		bytes, err := json.Marshal(products)
+
+		if err != nil {
+			panic(err)
+		}
+
+		database.Cache.Set(ctx, "products_backend", bytes, 30*time.Minute)
+	} else {
+		json.Unmarshal([]byte(result), &products)
+	}
+
+	var searchedProducts []models.Product
+
+	if s := c.Query("s"); s != "" {
+		lower := strings.ToLower(s)
+		for _, product := range products {
+			if strings.Contains(strings.ToLower(product.Title), lower) ||
+				strings.Contains(strings.ToLower(product.Description), lower) {
+				searchedProducts = append(searchedProducts, product)
+			}
+		}
+	} else {
+		searchedProducts = products
+	}
+
+	return c.JSON(searchedProducts)
 }
